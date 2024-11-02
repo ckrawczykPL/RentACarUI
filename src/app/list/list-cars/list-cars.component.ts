80d152e08ce2798a013c10a0481ec9dc89cd33cc
cezarykrawczyk <cezary.krawczyk@sidly.org>
2024-11-02 15:25:48 +0100

    added materials icons to global + content to list-cars component + example data in the TS

--- src/app/list/list-cars/list-cars.component.ts
+++ src/app/list/list-cars/list-cars.component.ts
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list-cars.component.css']
 })
 export class ListCarsComponent implements OnInit {
+  cars = [
+    { brand: 'Toyota', model: 'Corolla', year: 2015 },
+    { brand: 'Ford', model: 'Focus', year: 2018 },
+    { brand: 'Honda', model: 'Civic', year: 2020 },
+    { brand: 'BMW', model: 'X5', year: 2021 }
+  ];
 
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit(): void {
   }
-
 }
